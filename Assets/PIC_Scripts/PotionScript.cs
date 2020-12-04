27e9a820a4341a5ea7f85e822326c3ba19d90aaa
Pierre <p.cogne@rubika-edu.com>
2020-12-04 14:58:34 +0100

    Finished book potion updater

--- Assets/PIC_Scripts/PotionScript.cs
+++ Assets/PIC_Scripts/PotionScript.cs
@@ -8,14 +8,33 @@ namespace PotionCreationSystem
 
     public class PotionScript : MonoBehaviour
     {
-        Sprite icon;
-        Image potionImage;
-        List<Image> ingredientImages;
-        Potions potions;
+        public Image potionImage;
+        public List<Image> ingredientImages;
+        public Potions potions;
+
+        private void Start()
+        {
+            potionImage.sprite = potions.Icon;
+            potionImage.enabled = false;
+            foreach (Image image in ingredientImages)
+            {
+                image.enabled = false;
+            }
+        }
 
         // Update is called once per frame
         void Update()
         {
+            if (potions.isCrafted)
+            {
+                potionImage.enabled = true;
+                foreach (Image image in ingredientImages)
+                {
+                    image.enabled = true;
+                }
+                
+
+            }
         }
     }
 
